390e9ceb89047179c7cc56cc559cef18b51ecce0
agent-po <benjlai+agent-po@gmail.com>
2023-12-19 09:53:36 +1100

    Add new test cases for code changes tool

--- agency_server/tests/test_code_changes_tool.py
+++ agency_server/tests/test_code_changes_tool.py
@@ -55,6 +55,48 @@ class TestCodeChanges(unittest.TestCase):
         self.assertNotIn("    print('Hello, world!')\n", content)
         
     def tearDown(self):
+
+    def test_create_mode(self):
+        # Arrange
+        snippet = "print('New file created')"
+        test_file_path = 'new_test_file.py'
+
+        # Act
+        result = apply_code_changes(test_file_path, snippet, mode="create")
+
+        # Assert
+        self.assertTrue(os.path.exists(test_file_path))
+        with open(test_file_path, 'r') as f:
+            content = f.read().strip()
+        self.assertEqual(content, snippet)
+
+        # Cleanup
+        if os.path.exists(test_file_path):
+            os.remove(test_file_path)
+
+    def test_error_handling_patterns_not_found(self):
+        # Arrange
+        snippet = ""
+        start_pattern = "non_existent_start_pattern"
+        end_pattern = "non_existent_end_pattern"
+
+        # Act
+        result = apply_code_changes(self.test_file_path, snippet, start_pattern, end_pattern, mode="replace")
+
+        # Assert
+        self.assertIn("pattern not found", result.lower())
+
+    def test_error_handling_invalid_mode(self):
+        # Arrange
+        snippet = ""
+        mode = "invalid_mode"
+
+        # Act
+        result = apply_code_changes(self.test_file_path, snippet, mode=mode)
+
+        # Assert
+        self.assertIn("invalid mode", result.lower())
+
         # Clean up the test file
         if os.path.exists(self.test_file_path):
             os.remove(self.test_file_path)
